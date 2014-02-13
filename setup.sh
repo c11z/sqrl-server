b065e2796b153fb40174d12db005e51665dadef7
Cory Dominguez <corydominguez@gmail.com>
2014-02-12 18:37:54 -0800

    another tweak to setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 sudo mkdir logs
-sudo cp ~/secret.py gator/
+sudo cp /home/coryd/secret.py /var/www/html/gator-server/gator/
 cd ../
 sudo chown -R apache:apache gator-server
 sudo chmod  -R 755 gator-server
